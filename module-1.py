f691e94ae896580de415dfbf49b03348d08861a7
SID-7801 <117830359+SID-7801@users.noreply.github.com>
2023-10-21 15:35:03 +0530

    Add files via upload

---
+++ module-1.py
@@ -0,0 +1,36 @@
+Why Django should be used for web-development? Explain how you can 
+create a project in Django? 
+Django is an open source high-level Python Web framework that encourages rapid development and clean, pragmatic design
+Step 1: Opening PowerShell.
+Step 2: Verifying Python Installation.
+Step 3: Upgrading PIP.
+Step 4: Creating a Project Directory.
+Step 5: Creating the Virtual Environment.
+Step 7: Installing Django.
+Step 8: Create the Django Project.
+Step 9: Running the Development Server.
+
+
+How to check installed version of django? 
+To check the version, open your terminal or command prompt and enter the following command: django-admin --version 
+
+Mention what command line can be used to load data into Django? 
+How do you load data in Django?
+You can load data by calling manage.py loaddata <fixturename> , where <fixturename> is the name of the fixture file you've created. Each time you run loaddata , the data will be read from the fixture and reloaded into the database.
+
+
+Explain what does django-admin.py make messages command is used for?
+The admin.py file is used to display your models in the Django admin panel. You can also customize your admin panel. Hope this helps you. Django has a builtin admin interface that reads metadata from your models, such as fields, and lets you perform CRUD operations for free.
+
+
+What is a QuerySet?
+A QuerySet is a collection of data from a database. A QuerySet is built up as a list of objects. QuerySets makes it easier to get the data you actually need, by allowing you to filter and order the data at an early stage.
+
+
+Mention what command line can be used to load data into Django? 
+ou can load data by calling manage.py loaddata <fixturename> , where <fixturename> is the name of the fixture file you've created. Each time you run loaddata , the data will be read from the fixture and reloaded into the database.
+
+
+
+
+
